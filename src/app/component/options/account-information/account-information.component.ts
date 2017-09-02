6ab19eddd98bb4fc1f0c1be11898ae7dc79a3ee8
ledamint <heresliker@gmail.com>
2017-09-02 23:08:50 +0700

    add promt to change automail

--- src/app/component/options/account-information/account-information.component.ts
+++ src/app/component/options/account-information/account-information.component.ts
@@ -8,15 +8,19 @@ import { AccountInformationService } from 'app/service/account-information.servi
   template: `
         <form class="form" #emailForm="ngForm" action="" method="post" (ngSubmit)="updateEmail(emailForm.value.email)">
             <h3>Email</h3>
-            <input class="text-input" type="email" name="email" placeholder="email" focus="true"
-              [ngModel]="accountInformationService.email" required email>
+            <div class="input-wrapper">
+                <input class="text-input" type="email" name="email" placeholder="email" focus="true"
+                  [ngModel]="accountInformationService.email" required email>
+                <span class="prompt theme-color-text-second-color" [hidden]="emailForm.value.email.search('testmail') === -1">
+                  change to your email if you want to save your progress</span>
+            </div>
             <button class="button" type="submit" [disabled]="!emailForm.valid">Change email</button>
         </form>
         <form class="form" #passwordForm="ngForm" action="" method="post" (ngSubmit)="changePassword(passwordForm.value.password)">
             <h3>Password</h3>
             <div class="input-wrapper">
                 <input class="text-input" type="password" name="password" placeholder="password" ngModel required minlength="6">
-                <span class="prompt">min 6 symbols</span>
+                <span class="prompt theme-color-text-second-color">min 6 symbols</span>
             </div>
             <input class="text-input" type="password" name="confirm-password" placeholder="confirm password" ngModel required>
             <button class="button" type="submit" [disabled]="!passwordForm.valid ||
