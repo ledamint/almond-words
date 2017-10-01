ef9780c89abe4249aad7d38fe7ad806493544e65
ledamint <heresliker@gmail.com>
2017-10-01 13:43:08 +0700

    add decreasing words one time in a date

--- server/db-actions/words.js
+++ server/db-actions/words.js
@@ -8,34 +8,48 @@ const decreaseWordsKnowledge = (db, userId) => {
       } else if (user === null) {
         console.log('not found');
       } else {
-        const words = user.boards[user.activeBoard].words.map((word) => {
-          if (word.decreaseTime !== undefined) {
-            if (typeof word.decreaseTime.time === 'string') word.decreaseTime.time = new Date(word.decreaseTime.time);
-
-            if (word.decreaseTime.time !== null && word.decreaseTime.time < new Date()) {
-              word.knowledge -= 1;
-              word.decreaseTime.time = null;
-
-              return word;
-            }
-          }
-
-          return word;
-        });
-
         const query = {
-          $set: {
-            [`boards.${user.activeBoard}.words`]: words,
-          },
+          $set: { },
         };
+        const nextDecreaseWordsTime = new Date();
+        // TODO: move sum of + day to config
+        nextDecreaseWordsTime.setDate(new Date().getDate() + 1);
 
-        usersCollection.findOneAndUpdate(userId, query, (err) => {
-          if (err) {
-            console.log(err);
-          }
+        if (user.nextDecreaseWordsTime instanceof Date && user.nextDecreaseWordsTime < new Date()) {
+          const words = user.boards[user.activeBoard].words.map((word) => {
+            if (word.decreaseTime !== undefined) {
+              if (typeof word.decreaseTime.time === 'string') word.decreaseTime.time = new Date(word.decreaseTime.time);
 
+              if (word.decreaseTime.time !== null && word.decreaseTime.time < new Date()) {
+                word.knowledge -= 1;
+                word.decreaseTime.time = null;
+
+                return word;
+              }
+            }
+
+            return word;
+          });
+
+          query.$set[`boards.${user.activeBoard}.words`] = words;
+          query.$set.nextDecreaseWordsTime = nextDecreaseWordsTime;
+        }
+
+        if (user.nextDecreaseWordsTime === undefined) {
+          query.$set.nextDecreaseWordsTime = nextDecreaseWordsTime;
+        }
+
+        if (Object.keys(query.$set).length !== 0) {
+          usersCollection.findOneAndUpdate(userId, query, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              resolve();
+            }
+          });
+        } else {
           resolve();
-        });
+        }
       }
     });
   });
