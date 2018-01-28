538fadb00e8f676db77b557d38e094d6e029484c
Leroy Fer <heresliker@gmail.com>
2018-01-28 22:34:18 +0300

    add link to yandex with the right word

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -14,6 +14,7 @@ function translateWord(word) {
   const apiUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
   const apiKey = 'trnsl.1.1.20170910T052245Z.2dcffa636619250a.1ae7a58bd70134c42f7ab907150584f2ed488d8f';
 
+  // TODO change ru to different languages
   return axios.get(
     `${apiUrl}?key=${apiKey}&text=${word}&lang=ru`);
 }
