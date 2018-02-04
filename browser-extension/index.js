b621af5240f4ad5e8a65b0b4c61ad661881a4250
Leroy Fer <heresliker@gmail.com>
2018-02-04 18:10:05 +0300

    add getting user languages

--- browser-extension/index.js
+++ browser-extension/index.js
@@ -1,18 +1,18 @@
 /* eslint-disable */
 
 axios.post('http://almondwords.com/login')
-    .then(function (res) {
-      if (res.data) {
-        document.body.querySelector('.content').innerHTML =
-          '<p class="login success">You have already logged in to <a href="http://almondwords.com" target="_blank">almondwords</a> and can add new words</p>';
-      } else {
-        document.body.querySelector('.content').innerHTML =
-          '<p class="login error">Please log in to <a href="http://almondwords.com" target="_blank">almondwords</a></p>';
-      }
-    })
-    .catch(function (err) {
-      document.body.removeChild(document.querySelector('.success'));
-    });
+  .then(function (res) {
+    if (res.data) {
+      document.body.querySelector('.content').innerHTML =
+        '<p class="login success">You have already logged in to <a href="http://almondwords.com" target="_blank">almondwords</a> and can add new words</p>';
+    } else {
+      document.body.querySelector('.content').innerHTML =
+        '<p class="login error">Please log in to <a href="http://almondwords.com" target="_blank">almondwords</a></p>';
+    }
+  })
+  .catch(function (err) {
+    document.body.removeChild(document.querySelector('.success'));
+  });
 
 var wordForm = document.forms[0];
 
