7ccfccc3321300a57fbb5dda3671916ce02612a5
Leroy Fer <heresliker@gmail.com>
2018-02-04 18:10:05 +0300

    add getting user languages

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,4 +1,16 @@
 /* eslint-disable */
+var familiarLanguage;
+var learningLanguage;
+
+function setUserLanguages() {
+  return axios.get('http://almondwords.com/user')
+    .then(function (res) {
+      familiarLanguage = res.data.familiarLanguage;
+      learningLanguage = res.data.learningLanguage;
+    });
+}
+
+setUserLanguages();
 
 chrome.extension.onRequest.addListener(function (wordData, MessageSender) {
   axios.post('http://almondwords.com/words', wordData)
@@ -14,18 +26,24 @@ function translateWord(word) {
   const apiUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
   const apiKey = 'trnsl.1.1.20170910T052245Z.2dcffa636619250a.1ae7a58bd70134c42f7ab907150584f2ed488d8f';
 
-  // TODO change ru to different languages
-  return axios.get(
-    `${apiUrl}?key=${apiKey}&text=${word}&lang=ru`);
+  if (familiarLanguage === undefined) {
+    return setUserLanguages()
+      .then(function () {
+        return axios.get(
+          `${apiUrl}?key=${apiKey}&text=${word}&lang=${learningLanguage}-${familiarLanguage}`);
+      });
+  } else {
+    return axios.get(
+      `${apiUrl}?key=${apiKey}&text=${word}&lang=${learningLanguage}-${familiarLanguage}`);
+  }
 }
 
 chrome.runtime.onMessage.addListener(function (word, MessageSender) {
   translateWord(word)
-    .then(
-      (res) => {
+    .then(function (res)  {
         chrome.tabs.sendMessage(MessageSender.tab.id, res.data.text[0]);
       })
-    .catch((err) => {
+    .catch(function (err) {
       chrome.tabs.sendMessage(MessageSender.tab.id, err);
     })
 });
