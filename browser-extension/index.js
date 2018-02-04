a6c54da185306679f5fa4545ece2f2955071f2c4
Leroy Fer <heresliker@gmail.com>
2018-02-04 16:51:46 +0300

    add checking access in popup

--- browser-extension/index.js
+++ browser-extension/index.js
@@ -1,5 +1,17 @@
 /* eslint-disable */
 
+axios.post('http://almondwords.com/login')
+    .then(function (res) {
+      if (res.data) {
+        document.body.removeChild(document.querySelector('.error'));
+      } else {
+        document.body.removeChild(document.querySelector('.success'));
+      }
+    })
+    .catch(function (err) {
+      document.body.removeChild(document.querySelector('.success'));
+    });
+
 var wordForm = document.forms[0];
 
 wordForm.addEventListener('submit', function (e) {
