2a3112fc74e761101ec25877c57d828c805931e8
Leroy Fer <heresliker@gmail.com>
2018-02-18 20:52:13 +0300

    add hiding and logo

--- src/app/component/welcome/welcome.component.ts
+++ src/app/component/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { setTimeout } from 'timers';
 
 @Component({
   selector: 'welcome',
   template: `
-    <div class="welcome">
+    <div class="welcome" [class.inactive]="isOpacityInactive" *ngIf="isShowing">
       <div class="content">
         <p>
           Привет!
@@ -22,16 +23,23 @@ import { Component } from '@angular/core';
           Автоматически фильтрует слова на новые и которые нужно повторить. Время повторения рассчитывается по наиболее эффективной методике, позволяющей легко и непринужденно закреплять слова в памяти.
         </p>
         <p>
-          Обладает автотестом, подбирающим уровень теста на основании уровня владения слова.
+          Обладает автотестом, подбирающим уровень теста на основании уровня владения слов.
         </p>
         <p>
-          Также ты сможешь пользоваться <a href="https://chrome.google.com/webstore/detail/almondwords/nmegbabcobddpflndffbclmfdbfhjbjp" target="_blank">расширением для браузера</a>, которое позволяет переводить и добавлять слова на любых сайтах в твой личный словарь.
+          Также ты сможешь пользоваться <a href="https://chrome.google.com/webstore/detail/almondwords/nmegbabcobddpflndffbclmfdbfhjbjp" target="_blank">расширением для браузера <img class="logo" src="assets/img/icon.png"></a>, которое позволяет переводить и добавлять слова на любых сайтах в твой личный словарь.
         </p>
+        <button class="button theme-color-text-main" (click)="hideWelcome()">Уже регистрируюсь!</button>
       </div>
     </div>
   `,
   styleUrls: [ './welcome.component.scss' ]
 })
 export class WelcomeComponent {
+  isOpacityInactive: boolean = false;
+  isShowing: boolean = true;
 
+  hideWelcome() {
+    this.isOpacityInactive = true;
+    setTimeout(() => this.isShowing = false, 1000);
+  }
 }
