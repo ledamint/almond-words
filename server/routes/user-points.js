3915462e03c69d374e720ffc3f021811d84d736f
Leroy Fer <heresliker@gmail.com>
2018-03-09 13:39:15 +0300

    add rules to common registration

--- server/routes/user-points.js
+++ server/routes/user-points.js
@@ -9,22 +9,24 @@ module.exports = (app, db) => {
     const pointsDifference = req.body.pointsDifference;
 
     db.collection('users').findOne(userId, (err, user) => {
-      let todayPoints = user.userPoints !== undefined ? user.userPoints.todayPoints : 0;
-      let allPoints = user.userPoints !== undefined ? user.userPoints.allPoints : 0;
+      if (user !== undefined) {
+        let todayPoints = user.userPoints !== undefined ? user.userPoints.todayPoints : 0;
+        let allPoints = user.userPoints !== undefined ? user.userPoints.allPoints : 0;
 
-      todayPoints += pointsDifference;
-      allPoints += pointsDifference;
-      todayPoints = todayPoints < 0 ? 0 : todayPoints;
-      allPoints = allPoints < 0 ? 0 : allPoints;
+        todayPoints += pointsDifference;
+        allPoints += pointsDifference;
+        todayPoints = todayPoints < 0 ? 0 : todayPoints;
+        allPoints = allPoints < 0 ? 0 : allPoints;
 
-      updateUser(db, userId, { userPoints: { ...user.userPoints, todayPoints, allPoints } })
-        .then((updatedUser) => {
-          res.send(updatedUser.userPoints);
-        })
-        .catch((err) => {
-          if (err === 404) res.sendStatus(404);
-          else if (err === 500) res.sendStatus(500);
-        });
+        updateUser(db, userId, { userPoints: { ...user.userPoints, todayPoints, allPoints } })
+          .then((updatedUser) => {
+            res.send(updatedUser.userPoints);
+          })
+          .catch((err) => {
+            if (err === 404) res.sendStatus(404);
+            else if (err === 500) res.sendStatus(500);
+          });
+      }
     });
   });
 };
